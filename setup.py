878c11c0b2b9d8e3d10c7a52088a9eebd76ccbcb
kingel <franklin@fourdigits.nl>
2012-06-26 13:18:17 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='collective.cookiecuttr',
+      version=version,
+      description="",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='gpl',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['collective', ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      extras_require={'test': ['plone.app.testing']},
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["templer.localcommands"],
+      )
---
+++ src/collective/cookiecuttr/testing.py
@@ -0,0 +1,27 @@
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import applyProfile
+
+from zope.configuration import xmlconfig
+
+class CollectiveCookiecuttr(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE, )
+
+    def setUpZope(self, app, configurationContext):
+        # Load ZCML for this package
+        import collective.cookiecuttr
+        xmlconfig.file('configure.zcml',
+                       collective.cookiecuttr,
+                       context=configurationContext)
+
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'collective.cookiecuttr:default')
+
+COLLECTIVE_COOKIECUTTR_FIXTURE = CollectiveCookiecuttr()
+COLLECTIVE_COOKIECUTTR_INTEGRATION_TESTING = \
+    IntegrationTesting(bases=(COLLECTIVE_COOKIECUTTR_FIXTURE, ),
+                       name="CollectiveCookiecuttr:Integration")
