9873c08eb0ebf6caa9c989a3d67edecac8efe744
Franklin Kingma <franklin@fourdigits.nl>
2012-11-08 10:14:03 +0100

    Fixed ga viewlet rendering

--- src/collective/cookiecuttr/browser/viewlet.py
+++ src/collective/cookiecuttr/browser/viewlet.py
@@ -42,12 +42,15 @@ class CookieCuttrViewlet(BrowserView):
 class CookieCuttrAwareAnalyticsViewlet(AnalyticsViewlet):
 
     def render(self):
+        import pdb; pdb.set_trace( )
         settings = getUtility(IRegistry).forInterface(ICookieCuttrSettings)
 
         available = settings and settings.cookiecuttr_enabled
 
         # Render if CookieCuttr is enabled and Cookies were accepted
-        if available and self.request.cookies.get('cc_cookie_accept', None):
+        if available and \
+            self.request.cookies.get('cc_cookie_accept', None) == \
+                'cc_cookie_accept':
             return super(CookieCuttrAwareAnalyticsViewlet, self).render()
 
         return ""
